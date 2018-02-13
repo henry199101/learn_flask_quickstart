d22e64e8b60dc3a39aeb69aeb7e6dddf0333afb1
henry199101 <331243543@qq.com>
2018-02-13 12:46:09 +0800

    Add user routing, URL is /user/<username>. URL中的<variable_name>是变量部分，这个部分中将会作为命名参数传递到你的函数。

--- hello.py
+++ hello.py
@@ -9,5 +9,10 @@ def index():
 def hello():
 	return 'Hello World'
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+	# show the user profile for that user
+	return 'User %s' % username
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
